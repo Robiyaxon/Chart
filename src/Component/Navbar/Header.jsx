fbad220d6899bcf661fd87fa486fb7bef16ce207
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2021-12-09 20:16:40 +0500

    initial commit

--- src/Component/Navbar/Header.jsx
+++ src/Component/Navbar/Header.jsx
@@ -1,19 +1,15 @@
 import clsx from 'clsx'
 import React from 'react'
-
 import MenuIcon from '@material-ui/icons/Menu'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
-
 import SimpleBadge from './Badge'
 import flag from "../../picture/flag.png"
 import LetterAvatars from '../Header/Avatar/Avatar'
 import SearchButton from '../Header/HeaderSearch/HeaderSearch'
-
 import style from './Navbar.module.css'
-
 export function Header(classes, open, handleDrawerOpen) {
   return <AppBar
     position="fixed"
